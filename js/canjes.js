344907bab68f5fdd34ff59646823885bac167721
lucas sosa <sosalucas906@gmail.com>
2024-01-11 11:10:27 -0300

    retoques funcion canjear puntos

--- js/canjes.js
+++ js/canjes.js
@@ -11,27 +11,65 @@ let puntosContainer = document.querySelector('.misPuntos')
 puntosContainer.innerText = `Puntos: ${puntos}`
 let containerCanje = document.querySelector('.posiblesCanjes')
 
+actualizarProdcutos()
 
-for (productos in arrayProductos) {
-    if (puntos >= arrayProductos[productos].puntos) {
-        containerCanje.innerHTML += `
+    botonCanje = containerCanje.querySelectorAll('.canjearProducto')
+    botonCanje.forEach((boton, index) => {
+        boton.addEventListener('click', () => {
+            console.log(posiblesCanjes[index].puntos)
+            puntos -= posiblesCanjes[index].puntos
+            localStorage.setItem('puntos', puntos)
+            puntosContainer.innerText = `Puntos: ${puntos}`
+            containerCanje.innerHTML = ''
+            actualizarProdcutos()
+            
+        })
+    })
+
+// actualizarProdcutos()
+
+function actualizarProdcutos() {
+    for (productos in arrayProductos) {
+        if (puntos >= arrayProductos[productos].puntos) {
+            containerCanje.innerHTML += `
             <article class="card">
-                <div>
-                    <img src="./assets/img/banking (1) (1).png" alt="foto random">
+            <div>
+            <img src="./assets/img/banking (1) (1).png" alt="foto random">
                 </div>
                 <h2>${arrayProductos[productos].categoria}</h2>
-                <button>${arrayProductos[productos].puntos}</button>
-            </article>
-        `
-        console.log(`Puedes canjear tus puntos por ${arrayProductos[productos].info}.`)
-        posiblesCanjes.push(arrayProductos[productos])
-    } 
-    // else if (puntos < 100) {
-    //     containerCanje.innerHTML += `<h2>Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más dinero para incrementar sus puntos</h2>`
-    //     console.log('Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más plata para incrementar sus puntos.')
-        
-    // }
+                <input type="submit" class="canjearProducto">${arrayProductos[productos].puntos}
+                </article>
+                `
+            console.log(`Puedes canjear tus puntos por ${arrayProductos[productos].info}.`)
+            posiblesCanjes.push(arrayProductos[productos])
+        }
+    }
+    if (puntos < 100) {
+        containerCanje.innerHTML += `<h2>Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más dinero para incrementar sus puntos</h2>`
+        console.log('Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más plata para incrementar sus puntos.')
+    }
+    
 }
+
+
+// function canjear() {
+//     botonCanje = containerCanje.querySelectorAll('.canjearProducto')
+//     botonCanje.forEach((boton, index) => {
+//         boton.addEventListener('click', () => {
+//             console.log(posiblesCanjes[index].puntos)
+//             puntos -= posiblesCanjes[index].puntos
+//             localStorage.setItem('puntos', puntos)
+//             puntosContainer.innerText = `Puntos: ${puntos}`
+//             containerCanje.innerHTML = ''
+//             actualizarProdcutos()
+            
+//         })
+//     })
+// }
+
+
+
+
 // else if (puntos < 100) {
 //         alert('Tus puntos son insuficientes para realizar canjes. Por favor, ingrese más plata para incrementar sus puntos.')
 //         seleccionarOpcion()
